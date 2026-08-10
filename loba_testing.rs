f4d4b06464b2138b5abbef063ecb0bc5fe384ab5
hinelo <88257418+hinelo@users.noreply.github.com>
2026-08-10 00:34:59 -0300

    Agora as funções são escritas dinamicamente, se não usar, não é escrito e poupa espaço de codigo (se isso demora mais ou menos para compilar, não tenho certeza)
    o que falta pro loba_testing virar só o loba? alias!, basicamente a unica coisa faltando para subistituir a versão antiga é o fn = função o que ja torna essa versão mais usavel (e provavelmente mais robusta por não ser baseado em replace)

--- loba_testing.rs
+++ loba_testing.rs
@@ -3,29 +3,76 @@ use std::io::{stdin,stdout,Write};
 use std::env;
 use std::fs;
 
-
-let _use_std: Vec<String>;
-let use_functions = true;
+let mut use_std: Vec<String> = vec![];
+let mut use_functions: Vec<String> = vec![];
 let use_main = false;
-let use_debug = true;
+let mut use_debug = true;
 let mut debug = String::new();
-
-
-
 let args: Vec<String> = env::args().collect();
-let data =  fs::read_to_string(args[1].clone()).expect("Arquivo não encontrado");
+let mut file_name = String::new();
 
-let mut criar = ||{
+for n in &args{
+	println!("{:?}", args);
+	if n.ends_with(".lb") {
+		file_name = n.to_string();		
+	} else if n.to_string() ==  "-D" ||  n.to_string() == "-d" {
+		use_debug = true;
+	}
+}
+if file_name.is_empty() {
+	println!("Adicione ao comando, qual arquivo executar");
+	return
+}
+
+let data =  fs::read_to_string(&file_name).expect("Arquivo não encontrado");
+
+if data.trim().to_lowercase().contains("pergunte") {
+	println!("pergunte!");
+	use_std.push("std::io::{stdin,stdout,Write};".to_string());
+	use_functions.push("pergunte".to_string());
+}
+
+if data.trim().to_lowercase().contains("draw") {
+	use_functions.push("draw".to_string());
+}
+
+if data.trim().to_lowercase().contains("diga!") {
+	use_functions.push("diga".to_string());
+}
+
+let criar = ||{
 	let mut output = String::new();
 	debug += "----criando----\n";
-	let temp_std = "use std::io::{stdin,stdout,Write};\n";
-	output += temp_std;
-	if use_functions {
-		let pergunte =  fs::read_to_string("cmd/pergunte").expect("Arquivo não encontrado");
-		let diga =  fs::read_to_string("cmd/diga").expect("Arquivo não encontrado");
-		let draw =  fs::read_to_string("cmd/draw").expect("Arquivo não encontrado");
-		output += &(pergunte+&diga+&draw);
-	} 
+	if use_std.is_empty() {
+		debug += "use_std = false\n";
+	} else {
+		debug += &format!("use_std = {:?}\n",use_std).to_string();
+		for n in use_std {
+			output+= &n;
+		}
+	}
+	if use_functions.is_empty() {
+		debug += "use_functions = false\n";
+	} else {
+		debug += "use_functions = ";
+		for n in use_functions {
+			if n == "pergunte"{
+				let pergunte =  fs::read_to_string("cmd/pergunte").expect("Arquivo não encontrado");
+				output += &pergunte;
+				debug += " pergunte";
+			}
+			if n == "diga"{
+				output += &fs::read_to_string("cmd/diga").expect("Arquivo não encontrado");
+				debug += " diga";
+			}
+			if n == "draw"{
+				output += &fs::read_to_string("cmd/draw").expect("Arquivo não encontrado");
+				debug += " draw";
+			}
+		}
+		debug += "\n";
+	}
+
 	if use_main {
 		debug += "use_main = true\n";
 	} else {
@@ -39,8 +86,13 @@ let mut criar = ||{
 		println!("{}", debug)
 	}
 
-	fs::write("script.rs", output).expect("Falha ao gravar o arquivo");
+	fs::write(&file_name.replace(".lb", ".rs"), output).expect("Falha ao gravar o arquivo");
 };	
 
 criar()
+
+
+
+
+
 }
