e6d70e00134fd5105de2df9e0033544a210fab41
hinelo <88257418+hinelo@users.noreply.github.com>
2026-08-10 14:07:21 -0300

    Agora oficialmente funcionando! corrigido problema com path e etc, era burrice minha na hora de compilar os comandos do programa.
    tem incompatibilidade com versões antigas, diga! e pergunte foram mudados levemente isso não é exatamente bug, mas espero arrumar o diga! com um alias.
    
    um bug que pretendo arrumar é que escrever função, ou qualquer outro metodo do loba em uma string faz ele ser efetivamente lido como usado, então função por exemplo numa string "em função dê" seria substituido por "em fn dê" embora se essa string não aparecer no codigo, como lido de arquivo e inputs, ela não vai ser substituida

---
+++ base/base_tester.rs
@@ -0,0 +1,56 @@
+use std::io::{stdin,stdout,Write};
+
+macro_rules! diga {
+    () => {
+        println!();
+    };
+
+    (? $head:expr, $($tail:tt)*) => {
+        print!("{:?} ", $head);    
+        digas!($($tail)*);  
+    };
+
+    (? $head:expr) => {
+        println!("{:?}", $head);
+    };
+
+    ($head:expr, $($tail:tt)*) => {
+        print!("{} ", $head);      
+        diga!($($tail)*);  
+    };
+
+    ($head:expr) => {
+        println!("{}", $head);
+    };
+}
+
+fn pergunte(conteudo: &str) -> String{
+	let mut variavel =String::new(); 
+	print!("{}\n", conteudo);
+	let _ =stdout().flush(); 
+	let _ = stdin().read_line(&mut variavel);
+	return variavel.replace("\n", "")
+}
+
+fn draw<const L: usize, const C: usize>(matrix: &[[u8; C]; L]) {
+    for double in matrix.chunks(2) {
+        if double.len() == 2 {
+            for (top, bot) in double[0].iter().zip(double[1].iter()) {
+                 print!("\x1b[38;5;{}m\x1b[48;5;{}m▀", top, bot);
+            }
+            print!("\x1b[0m\n");
+        } else {
+            for top in double[0].iter() {
+                 print!("\x1b[38;5;{}m▀", top);
+            }
+            print!("\x1b[0m\n");
+        }   
+    }
+}
+
+fn main() {
+	let mut state = [[0u8; 5];7];
+	state[0][1] = 2;
+    state[1][0] = 2;
+	draw(&state);
+}
--- loba_testing.rs
+++ loba_testing.rs
@@ -1,42 +1,102 @@
+use std::io::{stdin,stdout,Write}; 
+fn pergunte(conteudo: &str) -> String{
+	let mut variavel =String::new(); 
+	print!("{}\n", conteudo);
+	let _ =stdout().flush(); 
+	let _ = stdin().read_line(&mut variavel);
+	return variavel.replace("\n", "")
+}
+fn draw<const L: usize, const C: usize>(matrix: &[[u8; C]; L]) {
+    for double in matrix.chunks(2) {
+        if double.len() == 2 {
+            for (top, bot) in double[0].iter().zip(double[1].iter()) {
+                 print!("\x1b[38;5;{}m\x1b[48;5;{}m▀", top, bot);
+            }
+            print!("\x1b[0m\n");
+        } else {
+            for top in double[0].iter() {
+                 print!("\x1b[38;5;{}m▀", top);
+            }
+            print!("\x1b[0m\n");
+        }   
+    }
+}
+macro_rules! diga {
+    () => {
+        println!();
+    };
+
+    (? $head:expr, $($tail:tt)*) => {
+        print!("{:?} ", $head);    
+        diga!($($tail)*);  
+    };
+
+    (? $head:expr) => {
+        println!("{:?}", $head);
+    };
+
+    ($head:expr, $($tail:tt)*) => {
+        print!("{} ", $head);      
+        diga!($($tail)*);  
+    };
+
+    ($head:expr) => {
+        println!("{}", $head)
+    };
+}
+
+
 fn main() {
-use std::io::{stdin,stdout,Write};
 use std::env;
 use std::fs;
+use std::process::Command;
 
 let mut use_std: Vec<String> = vec![];
 let mut use_functions: Vec<String> = vec![];
-let use_main = false;
-let mut use_debug = true;
+let mut use_main = false;
+let mut use_debug = false;
+let mut _sskeep_file = true;
 let mut debug = String::new();
 let args: Vec<String> = env::args().collect();
 let mut file_name = String::new();
 
 for n in &args{
-	println!("{:?}", args);
 	if n.ends_with(".lb") {
 		file_name = n.to_string();		
 	} else if n.to_string() ==  "-D" ||  n.to_string() == "-d" {
 		use_debug = true;
+	} else if n.to_string() ==  "-F" ||  n.to_string() == "-f" {
+		use_debug = true;
 	}
 }
+
 if file_name.is_empty() {
-	println!("Adicione ao comando, qual arquivo executar");
+	diga!("Adicione ao comando, qual arquivo executar");
 	return
 }
 
-let data =  fs::read_to_string(&file_name).expect("Arquivo não encontrado");
+let cwd = std::env::current_dir().unwrap();
+let data = fs::read_to_string(cwd.join(&file_name)).unwrap_or_else(|erro| {
+    panic!(
+        "Arquivo não encontrado em: {:?}\nMotivo: {}\n", 
+        cwd.join(&file_name), erro
+    );
+});
 
-if data.trim().to_lowercase().contains("pergunte") {
-	println!("pergunte!");
+if data.trim().contains("main(") {
+	use_main = false
+}
+
+if data.trim().contains("pergunte(") {
 	use_std.push("std::io::{stdin,stdout,Write};".to_string());
 	use_functions.push("pergunte".to_string());
 }
 
-if data.trim().to_lowercase().contains("draw") {
+if data.trim().contains("draw(") {
 	use_functions.push("draw".to_string());
 }
 
-if data.trim().to_lowercase().contains("diga!") {
+if data.trim().contains("diga!(") {
 	use_functions.push("diga".to_string());
 }
 
@@ -48,28 +108,28 @@ let criar = ||{
 	} else {
 		debug += &format!("use_std = {:?}\n",use_std).to_string();
 		for n in use_std {
-			output+= &n;
+			 output += &format!("use {} \n", n);
 		}
 	}
+
 	if use_functions.is_empty() {
 		debug += "use_functions = false\n";
 	} else {
 		debug += "use_functions = ";
 		for n in use_functions {
-			if n == "pergunte"{
-				let pergunte =  fs::read_to_string("cmd/pergunte").expect("Arquivo não encontrado");
-				output += &pergunte;
-				debug += " pergunte";
-			}
-			if n == "diga"{
-				output += &fs::read_to_string("cmd/diga").expect("Arquivo não encontrado");
-				debug += " diga";
-			}
-			if n == "draw"{
-				output += &fs::read_to_string("cmd/draw").expect("Arquivo não encontrado");
-				debug += " draw";
-			}
-		}
+	        if n == "pergunte" {
+	            output += include_str!("cmd/pergunte");
+	            debug += " pergunte";
+	        }
+	        if n == "diga" {
+	            output += include_str!("cmd/diga");
+	            debug += " diga";
+	        }
+	        if n == "draw" {
+	            output += include_str!("cmd/draw");
+	            debug += " draw";
+	        }
+	    }
 		debug += "\n";
 	}
 
@@ -81,18 +141,28 @@ let criar = ||{
 		output += &("fn main() {\n" .to_owned() + &data +"\n}" );
 
 	}
+
 	if use_debug {
 		debug += "----PRONTO-----";
-		println!("{}", debug)
+		diga!(debug);
 	}
 
 	fs::write(&file_name.replace(".lb", ".rs"), output).expect("Falha ao gravar o arquivo");
+	Command::new("rustc")
+			.arg(&file_name.replace(".lb", ".rs"))
+	        .spawn()
+	       	.expect("no rustc compiler");
+	/*
+    if keep_file == false {
+    	fs::remove_file(&file_name.replace(".lb", ".rs")).expect("Falha ao apagar o arquivo")
+
+    }
+
+    Command::new("./".to_owned() + &file_name.replace(".lb", ".rs"))
+        	.spawn()
+       		.expect("no file");
+    */
 };	
 
 criar()
-
-
-
-
-
 }
